b586ac68fda316ad436973fd360e690cf043d35b
Cesar Frick <cesarfrick@gmail.com>
2022-02-18 23:14:22 -0500

    Implement Bottom Nav Bar

---
+++ lib/screens/pages_wrapper.dart
@@ -0,0 +1,47 @@
+import 'package:apod_gallery/screens/favorites.dart';
+import 'package:apod_gallery/screens/home.dart';
+import 'package:flutter/material.dart';
+
+class Pages extends StatefulWidget {
+  const Pages({Key? key}) : super(key: key);
+
+  @override
+  _PagesState createState() => _PagesState();
+}
+
+class _PagesState extends State<Pages> {
+  int _currentIndex = 0;
+
+  final screens = const [
+    Home(),
+    Favorites(),
+  ];
+
+  void _updateIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(index: _currentIndex, children: screens),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          _updateIndex(index);
+        },
+        backgroundColor: Colors.pink,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white70,
+        showUnselectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), label: 'Favorites'),
+        ],
+      ),
+    );
+  }
+}
