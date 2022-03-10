a82b810c07de1805a28a8bd0d063945f66c97623
Cesar Frick <cesarfrick@gmail.com>
2022-03-10 00:56:34 -0500

    Improve filter and layout

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,7 +2,6 @@ import 'package:apod_gallery/provider/pics_notifier.dart';
 import 'package:apod_gallery/provider/pics_provider.dart';
 import 'package:flutter/material.dart';
 
-import 'package:apod_gallery/services/picture_service.dart';
 import 'package:apod_gallery/components/picture_card.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,6 +13,8 @@ class Home extends ConsumerStatefulWidget {
 }
 
 class _HomeState extends ConsumerState<Home> {
+  final _filterController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
@@ -40,20 +41,39 @@ class _HomeState extends ConsumerState<Home> {
     if (_pictures.loadState == LoadingState.isLoaded) {
       return CustomScrollView(slivers: <Widget>[
         SliverAppBar(
-          actions: const [
-            Icon(Icons.search),
+          pinned: false,
+          floating: true,
+          actions: [
+            _filterController.text.isEmpty
+                ? const Padding(
+                    padding: EdgeInsets.only(right: 10.0),
+                    child: Icon(Icons.search),
+                  )
+                : IconButton(
+                    icon: const Icon(Icons.close),
+                    onPressed: () {
+                      _filterController.clear();
+                      _pictures.restorePics();
+                    },
+                  ),
           ],
           backgroundColor: Colors.pinkAccent,
-          flexibleSpace: TextField(
-            style: const TextStyle(
-              color: Colors.white,
-              height: 2,
+          flexibleSpace: Padding(
+            padding: const EdgeInsets.only(left: 8.0, right: 16.0, top: 10.0),
+            child: TextField(
+              controller: _filterController,
+              decoration: null,
+              cursorColor: Colors.white,
+              autofillHints: _pictures.pics.map((pic) => pic.title),
+              style: const TextStyle(
+                color: Colors.white,
+                height: 2,
+              ),
+              onChanged: (text) {
+                _pictures.filterPics(text);
+              },
             ),
-            onChanged: (text) {
-              _pictures.filterPics(text);
-            },
           ),
-          pinned: true,
         ),
         _getContentWidget(_pictures),
       ]);
@@ -75,18 +95,21 @@ class _HomeState extends ConsumerState<Home> {
 
   Widget _getContentWidget(PicsNotifier _pictures) {
     return _pictures.pics.isNotEmpty
-        ? SliverGrid(
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200.0,
-              childAspectRatio: 1,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-            ),
-            delegate: SliverChildBuilderDelegate(
-              (context, index) => PictureCard(
-                picture: _pictures.pics[index],
+        ? SliverPadding(
+            padding: const EdgeInsets.only(top: 16),
+            sliver: SliverGrid(
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 200.0,
+                childAspectRatio: 1,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
               ),
-              childCount: _pictures.pics.length,
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => PictureCard(
+                  picture: _pictures.pics[index],
+                ),
+                childCount: _pictures.pics.length,
+              ),
             ),
           )
         : SliverFillRemaining(
